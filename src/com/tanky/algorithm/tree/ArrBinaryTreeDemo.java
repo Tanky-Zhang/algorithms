7850c313cf82cb2f2fa19f262c281a65d8bb685a
zhangzhongguo <zhangzhongguo@58.com>
2022-03-15 09:48:04 +0800

    feat:去除乱码

--- src/com/tanky/algorithm/tree/ArrBinaryTreeDemo.java
+++ src/com/tanky/algorithm/tree/ArrBinaryTreeDemo.java
@@ -1,39 +1,30 @@
 package com.tanky.algorithm.tree;
 
-//��дһ��ArrayBinaryTree, ʵ��˳��洢����������
 
 class ArrBinaryTree {
-	private int[] arr;//�洢���ݽ�������
 
-	public ArrBinaryTree(int[] arr) {
-		this.arr = arr;
-	}
-	
-	//����preOrder
-	public void preOrder() {
-		this.preOrder(0);
-	}
-	
-	//��дһ�����������˳��洢��������ǰ�����
-	/**
-	 * 
-	 * @param index ������±� 
-	 */
-	public void preOrder(int index) {
-		//�������Ϊ�գ����� arr.length = 0
-		if(arr == null || arr.length == 0) {
-			System.out.println("����Ϊ�գ����ܰ��ն�������ǰ�����");
-		}
-		//�����ǰ���Ԫ��
-		System.out.println(arr[index]); 
-		//����ݹ����
-		if((index * 2 + 1) < arr.length) {
-			preOrder(2 * index + 1 );
-		}
-		//���ҵݹ����
-		if((index * 2 + 2) < arr.length) {
-			preOrder(2 * index + 2);
-		}
-	}
-	
+    private int[] arr;
+
+    public ArrBinaryTree(int[] arr) {
+        this.arr = arr;
+    }
+
+    public void preOrder() {
+        this.preOrder(0);
+    }
+
+
+    public void preOrder(int index) {
+        if (arr == null || arr.length == 0) {
+            //System.out.println("����Ϊ�գ����ܰ��ն�������ǰ�����");
+        }
+        System.out.println(arr[index]);
+        if ((index * 2 + 1) < arr.length) {
+            preOrder(2 * index + 1);
+        }
+        if ((index * 2 + 2) < arr.length) {
+            preOrder(2 * index + 2);
+        }
+    }
+
 }
