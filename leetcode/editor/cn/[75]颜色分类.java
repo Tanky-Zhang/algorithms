d76eb70f02eca4363d404b1c7b32a08e6ce3a113
zhangzhongguo <zhangzhongguo@kuaishou.com>
2026-04-09 23:27:41 +0800

    feat:增加09题目

--- leetcode/editor/cn/[75]颜色分类.java
+++ leetcode/editor/cn/[75]颜色分类.java
@@ -2,7 +2,35 @@
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
     public void sortColors(int[] nums) {
-        
+
+        int p0 = 0;
+        int p1 = 0;
+        int p2 = nums.length - 1;
+
+        while (p1 <= p2) {
+            if (nums[p1] == 0) {
+                swap(nums, p0, p1);
+                p0++;
+            } else if (nums[p1] == 2) {
+                swap(nums, p1, p2);
+                p2--;
+            } else {
+                p1++;
+            }
+            if (p1<p0){
+                p1=p0;
+            }
+        }
+
+
+    }
+
+    public void swap(int[] nums, int i, int j) {
+
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
